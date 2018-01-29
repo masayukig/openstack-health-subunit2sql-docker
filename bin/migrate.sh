40d3309eec1beed7d61eda8b7c8d968275925d6a
Masayuki Igawa <masayuki@igawa.io>
2018-01-29 17:02:08 +0900

    Separate subunit stream files

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -4,12 +4,14 @@
 /venv/bin/subunit2sql-db-manage --config-file /app/etc/subunit2sql.conf upgrade head
 
 # load data
+mkdir -p /data/bak/v1
+mkdir -p /data/bak/v2
 for f in /data/* :
 do
-    mkdir -p /data/org
     if [ -f ${f} ]; then
         cat ${f} | /venv/bin/subunit-1to2 > ${f}.v2
-        mv ${f} /data/org/
+        mv ${f} /data/bak/v1/
+        /venv/bin/subunit2sql --config-file /app/etc/subunit2sql.conf /data/${f}.v2
+        mv ${f}.v2 /data/bak/v2/
     fi
 done
-/venv/bin/subunit2sql --config-file /app/etc/subunit2sql.conf /data/*
