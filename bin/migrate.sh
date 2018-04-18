5125fb2c942966ae102b6abae6174f92b8c84738
Masayuki Igawa <masayuki@igawa.io>
2018-04-18 16:47:09 +0900

    Fix migration script

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -6,12 +6,12 @@
 # load data
 mkdir -p /data/bak/v1
 mkdir -p /data/bak/v2
-for f in /data/* :
+for f in /data/*
 do
     if [ -f ${f} ]; then
         cat ${f} | /venv/bin/subunit-1to2 > ${f}.v2
         mv ${f} /data/bak/v1/
-        /venv/bin/subunit2sql --config-file /app/etc/subunit2sql.conf /data/${f}.v2
+        /venv/bin/subunit2sql --config-file /app/etc/subunit2sql.conf ${f}.v2
         mv ${f}.v2 /data/bak/v2/
     fi
 done
