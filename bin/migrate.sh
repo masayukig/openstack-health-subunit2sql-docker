dc5d3ec71a777c0b8ccffbe1cb3050b003eeceb8
Masayuki Igawa <masayuki@igawa.io>
2018-04-19 18:19:12 +0900

    Add artifacts parameter

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -5,6 +5,7 @@
 
 # load data
 RUN_META=${SUBUNIT2SQL_RUN_META:-project:openstack/tempest, build_name:default-build}
+ARTIFACTS=${SUBUNIT2SQL_ARTIFACTS:-http://example.com/}
 mkdir -p /data/bak/v1
 mkdir -p /data/bak/v2
 for f in /data/*
@@ -12,7 +13,7 @@ do
     if [ -f ${f} ]; then
         cat ${f} | /venv/bin/subunit-1to2 > ${f}.v2
         mv ${f} /data/bak/v1/
-        /venv/bin/subunit2sql --config-file /app/etc/subunit2sql.conf --run_meta "${RUN_META}" ${f}.v2
+        /venv/bin/subunit2sql --config-file /app/etc/subunit2sql.conf --run_meta "${RUN_META}" --artifacts "${ARTIFACTS}" ${f}.v2
         mv ${f}.v2 /data/bak/v2/
     fi
 done
